2cc0316c360ef2d88c1354955724fd6d329c4005
dylan33smith <dysm3436@colorado.edu>
2024-03-14 12:14:53 -0600

    added fj_url and sbh_url parameter to XDC func

--- xperimental-data-conv/xperimental_data_conv/main.py
+++ xperimental-data-conv/xperimental_data_conv/main.py
@@ -7,8 +7,8 @@ import requests
 import os
 
       
-#TODO change inputs to match new e2f requirements
-def experimental_data_uploader(file_path_in, fj_user, fj_pass, sbh_user,
+#TODO get rid of hard coded urls
+def experimental_data_uploader(file_path_in, fj_url, fj_user, fj_pass, sbh_url, sbh_user,
                                sbh_pass, sbh_collec, sbh_overwrite=False,
                                fj_overwrite=False):
 #Excel to SBOL
@@ -23,8 +23,10 @@ def experimental_data_uploader(file_path_in, fj_user, fj_pass, sbh_user,
      conv.converter(file_path_in, file_path_out, homespace=homespace)
 
      # SBH Login
+     sbh_login = sbh_url + "/login"
      response = requests.post(
-          'https://synbiohub.org/login',
+          # 'https://synbiohub.colorado.edu/login',
+          sbh_login,
           headers={'Accept': 'text/plain'},
           data={
                'email': sbh_user,
@@ -32,10 +34,13 @@ def experimental_data_uploader(file_path_in, fj_user, fj_pass, sbh_user,
                }
      )
      x_token = response.text
+     print(response)
 
      # Pull graph uri from synbiohub
+     sbh_profile = sbh_url + "/profile"
      response = requests.get(
-          'https://synbiohub.org/profile',
+          # 'https://synbiohub.colorado.edu/profile',
+          sbh_profile,
           headers={
                'Accept': 'text/plain',
                'X-authorization': x_token
@@ -44,13 +49,18 @@ def experimental_data_uploader(file_path_in, fj_user, fj_pass, sbh_user,
      sbol_graph_uri = response.json()['graphUri']
      sbol_collec_url = f'{sbol_graph_uri}/{sbh_collec}/'
 
+     print(sbol_graph_uri)
+     print(sbol_collec_url)
+
      # Parse sbol to create hashmap of flapjack id to sbol uri
      doc = sbol2.Document()
      doc.read(file_path_out)
 
+     fj_id = fj_url + "/ID"
      sbol_hash_map = {}
      for tl in doc:
-          if 'https://flapjack.rudge-lab.org/ID' in tl.properties:
+          # if 'https://flapjack.rudge-lab.org/ID' in tl.properties:
+          if fj_id in tl.properties:
                sbol_uri = tl.properties['http://sbols.org/v2#persistentIdentity'][0]
                sbol_uri = sbol_uri.replace(homespace, sbol_collec_url)
                sbol_uri = f'{sbol_uri}/1'
@@ -58,13 +68,15 @@ def experimental_data_uploader(file_path_in, fj_user, fj_pass, sbh_user,
                sbol_name = str(tl.properties['http://sbols.org/v2#displayId'][0])
                sbol_hash_map[sbol_name] = sbol_uri
 
+     print(sbol_hash_map)
+
+
+
      #Excel to Flapjack
                                      
      # upload the excel file to flapjack and get hash map back
-     fj_url = "flapjack.rudge-lab.org:8000" # this should be user input
-     # hash_map = e2f.flapjack_upload(fj_url, fj_user, fj_pass, file_path_in)
-     # X2F.upload_all(parameters) 
-     # hash_map = X2F.hash_map     
+     fj_url = fj_url + ":8000" # this should be user input
+   
 
      print("made it to fj portion")
 
@@ -90,7 +102,9 @@ def experimental_data_uploader(file_path_in, fj_user, fj_pass, sbh_user,
           if id in hash_map:
             setattr(tl, 'flapjack_ID',
                     sbol2.URIProperty(tl,
-                    'https://flapjack.rudge-lab.org/ID',
+                    # 'https://flapjack.rudge-lab.org/ID',
+                    fj_id,
+                    # what is the fj url in the line below?
                      '0', '*', [], initial_value=f'http://wwww.flapjack.com/{hash_map[id]}'))
      doc.write(file_path_out2)
 
@@ -99,8 +113,10 @@ def experimental_data_uploader(file_path_in, fj_user, fj_pass, sbh_user,
      else:
           sbh_overwrite = '0'
      # SBH file upload
+     sbh_submit = sbh_url + "/submit"
      response = requests.post(
-          'https://synbiohub.org/submit',
+          # 'https://synbiohub.colorado.edu/submit',
+          sbh_submit,
           headers={
                'Accept': 'text/plain',
                'X-authorization': x_token
--- xperimental-data-conv/xperimental_data_conv/temp2.py
+++ xperimental-data-conv/xperimental_data_conv/temp2.py
@@ -6,16 +6,18 @@ import os
 
 fj_user = "dylan33smith"
 fj_pass = "coco33"
-sbh_user = 'dylan33smith'
-sbh_pass = 'coco33'
+sbh_user = "dylan33smith"
+sbh_pass = "coco33"
+sbh_url = "https://synbiohub.colorado.edu"
+fj_url = "flapjack.rudge-lab.org"
 
 
 direct = os.path.split(__file__)[0]
 # file_path_in = os.path.join(direct, '..', 'xperimental-data-conv','tests','test_files', 'flapjack_excel_converter_v030.xlsx')
 # sbh_overwrite = '1'
-sbh_collec = 'Flapjack'
+sbh_collec = "Flapjack"
 file_path_in = "xdc_test.xlsx"
 
-sbol_collec_url = xdc.experimental_data_uploader(file_path_in, fj_user, fj_pass,
-                               sbh_user, sbh_pass, sbh_collec, sbh_overwrite=True,
+sbol_collec_url = xdc.experimental_data_uploader(file_path_in, fj_url, fj_user, fj_pass,
+                               sbh_url, sbh_user, sbh_pass, sbh_collec, sbh_overwrite=True,
                                fj_overwrite=True)
